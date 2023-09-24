367ed94f910063bf97d9dbe407458d188cb16561
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-09-24 22:07:36 +0530

    case list, case info, create case

--- src/controllers/caseController.js
+++ src/controllers/caseController.js
@@ -1,19 +1,40 @@
 import asyncHandler from "express-async-handler";
 import {
-  getChildProfilesAsync,
+  createCaseAsync,
+  getCaseListasync,
+  getCaseInfoByCaseIdasync,
 } from "../services/caseService.js";
 import { notFound } from "../middleware/errorMiddleware.js";
-
 
 // @desc notification broadcast
 // route POST /api/notifications/broadcast
 // @access Private
-export const getChildProfileList = asyncHandler(async (req, res) => {
-  // run query
-  const results = await getChildProfilesAsync();
-  // return response
+
+export const createCase = asyncHandler(async (req, res) => {
+  const caseName = req.body.caseName;
+  const state = req.body.state;
+  const childProfileID = req.body.childProfileID;
+  const caseOwnerID = req.body.caseOwnerID;
+  const createID = req.body.createID;
+  await createCaseAsync(caseName, state, childProfileID, caseOwnerID, createID);
   return res.status(200).json({
     success: true,
-    childProfiles: results
-  })
+    message: "successfully created a case",
+  });
+});
+
+export const getCaseList = asyncHandler(async (req, res) => {
+  const result = await getCaseListasync();
+  return res.status(200).json({
+    success: true,
+    caseList: result,
+  });
+});
+
+export const getCaseInfoByCaseId = asyncHandler(async (req, res) => {
+  const result = await getCaseInfoByCaseIdasync(req.body.caseId);
+  return res.status(200).json({
+    success: true,
+    caseInfo: result[0],
+  });
 });
---
+++ src/migrations/scripts/20230924.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "Case" ADD "description" varchar(255) NOT NULL DEFAULT '' ;
